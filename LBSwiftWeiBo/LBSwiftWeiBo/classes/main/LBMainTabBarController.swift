6cf678d82a59e32c214208d2a7f96dab5a2ad47d
liubo <liubo@LiuBo-Mac.local>
2017-12-04 23:48:56 +0800

    利用字符串创建控制器类

--- LBSwiftWeiBo/LBSwiftWeiBo/classes/main/LBMainTabBarController.swift
+++ LBSwiftWeiBo/LBSwiftWeiBo/classes/main/LBMainTabBarController.swift
@@ -12,13 +12,36 @@ class LBMainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        addChildViewController(childVc: LBHomeTabVController(), title: "首页", imageName: "tabbar_home")
-        addChildViewController(childVc: LBMessageTabVController(), title: "消息", imageName: "tabbar_message_center")
-        addChildViewController(childVc: LBDiscoverTabVController(), title: "发现", imageName: "tabbar_discover")
-        addChildViewController(childVc: LBProfileTabVController(), title: "我", imageName: "tabbar_profile")
+        print(LBHomeTabVController())
+//        addChildViewController(childVc: LBHomeTabVController(), title: "首页", imageName: "tabbar_home")
+//        addChildViewController(childVc: LBMessageTabVController(), title: "消息", imageName: "tabbar_message_center")
+//        addChildViewController(childVc: LBDiscoverTabVController(), title: "发现", imageName: "tabbar_discover")
+//        addChildViewController(childVc: LBProfileTabVController(), title: "我", imageName: "tabbar_profile")
+        addChildViewController(childVcStr: "LBHomeTabVController", title: "首页", imageName: "tabbar_home")
+        addChildViewController(childVcStr: "LBMessageTabVController", title: "消息", imageName: "tabbar_message_center")
+        addChildViewController(childVcStr: "LBDiscoverTabVController", title: "发现", imageName: "tabbar_discover")
+        addChildViewController(childVcStr: "LBProfileTabVController", title: "我", imageName: "tabbar_profile")
     }
-    private func addChildViewController(childVc: UIViewController, title : String, imageName : String) {
+    private func addChildViewController(childVcStr: String, title : String, imageName : String) {
+        
+        // 注意：如果用字符串拿到对应的控制器，<LBSwiftWeiBo.LBHomeTabVController: 0x7ff876c06c80>
+        // 那就需要在控制器名之前加上项目名字LBSwiftWeiBo + . +LBHomeTabVController，NSClassFromString("LBSwiftWeiBo.LBHomeTabVController")
+        guard let nameSpace = Bundle.main.infoDictionary!["CFBundleExecutable"] as? String else{
+            print("没有获取命名空间")
+            return
+        }
+        
+        guard let childVcClass = NSClassFromString(nameSpace + "." + childVcStr) else {
+            print("没有获取到字符串对应的Class")
+            return
+        }
+        
+        guard let childClass = childVcClass as? UIViewController.Type  else {
+            return
+        }
+        
+        /// 创建控制器对象
+        let childVc = childClass.init()
         
         /// 设置子控制器的属性
         childVc.title = title
