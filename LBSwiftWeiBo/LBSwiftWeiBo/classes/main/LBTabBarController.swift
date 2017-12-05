8b5b4773a471a953c524c5f154b249e401c12515
liubo <liubo@LiuBo-Mac.local>
2017-12-05 22:52:32 +0800

    添加标签栏中间按钮

--- LBSwiftWeiBo/LBSwiftWeiBo/classes/main/LBTabBarController.swift
+++ LBSwiftWeiBo/LBSwiftWeiBo/classes/main/LBTabBarController.swift
@@ -12,12 +12,15 @@ class LBTabBarController: UITabBarController {
 
     // 懒加载属性
     private lazy var imageNames = ["tabbar_home", "tabbar_message_center", "", "tabbar_discover", "tabbar_profile"]
+    lazy var composeBtn : UIButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupComposeBtn()
     }
     
     // 需要在viewWillAppear调整，不能在viewDidLoad调整
+    // 也可以在storyboard添加选中图片，中间按钮并且设置不可点击也可以在storyboard中实现
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -30,6 +33,24 @@ class LBTabBarController: UITabBarController {
             }
             item.selectedImage = UIImage(named: imageNames[i] + "_highlighted")
         }
+    }
+
+
+}
+
+extension LBTabBarController{
+
+    func setupComposeBtn() {
+        // 把composeBtn添加到tabbar中
+        tabBar.addSubview(composeBtn)
+        
+        composeBtn.setBackgroundImage(UIImage(named: "tabbar_compose_button"), for: .normal)
+        composeBtn.setBackgroundImage(UIImage(named: "tabbar_compose_button_highlighted"), for: .highlighted)
+        composeBtn.setImage(UIImage(named: "tabbar_compose_icon_add"), for: .normal)
+        composeBtn.setImage(UIImage(named: "tabbar_compose_icon_add_highlighted"), for: .highlighted)
+        composeBtn.sizeToFit()
+        // 设置位置
+        composeBtn.center = CGPoint(x: tabBar.center.x, y: tabBar.bounds.size.height * 0.5)
     }
 
 }
