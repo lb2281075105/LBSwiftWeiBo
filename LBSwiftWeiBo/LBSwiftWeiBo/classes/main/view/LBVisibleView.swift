b4912b8beb56601eb92df19722c3cb7f75025e6d
CodeJCSON <2334497007@qq.com>
2017-12-11 14:13:57 +0800

    访客视图中间图片旋转

--- LBSwiftWeiBo/LBSwiftWeiBo/classes/main/view/LBVisibleView.swift
+++ LBSwiftWeiBo/LBSwiftWeiBo/classes/main/view/LBVisibleView.swift
@@ -9,9 +9,34 @@
 import UIKit
 
 class LBVisibleView: UIView {
-
+    // 图片旋转
+    @IBOutlet weak var rotationView: UIImageView!
+    /// 图标
+    @IBOutlet weak var iconView: UIImageView!
+    /// 文本
+    @IBOutlet weak var tipLabel: UILabel!
     class func visitorView() -> LBVisibleView{
         return Bundle.main.loadNibNamed("LBVisibleView", owner: nil, options: nil)?.first as!LBVisibleView
     }
 
+    func setUpVisitorViewInfo(iconName : String, title : String) {
+        iconView.image = UIImage(named: iconName)
+        tipLabel.text = title
+        rotationView.isHidden = true
+    }
+    
+    func addRotationAnim() {
+        // 创建Z轴动画
+        let rotationAnim = CABasicAnimation(keyPath: "transform.rotation.z")
+        
+        // 设置动画的属性
+        rotationAnim.fromValue = 0
+        rotationAnim.toValue = Double.pi * 2
+        rotationAnim.repeatCount = MAXFLOAT
+        rotationAnim.duration = 5
+        rotationAnim.isRemovedOnCompletion = false
+        
+        // 将动画添加到layer中
+        rotationView.layer.add(rotationAnim, forKey: nil)
+    }
 }
